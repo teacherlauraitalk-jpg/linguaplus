53db723d7480fd429b05c0591705f1117df58842
Marco Fonseca <marcofonseca@MacBook-Air-de-Marco.local>
2025-12-16 22:23:34 -0300

    Preparando para deploy no Render

--- public/db.js
+++ public/db.js
@@ -1,11 +1,8 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    user: 'linguser',          // seu usuário do PostgreSQL
-    host: 'localhost',          // geralmente localhost
-    database: 'linguaplus',     // nome do banco
-    password: '',               // coloque a senha do linguser se houver
-    port: 5432                  // porta padrão do PostgreSQL
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
 });
 
 module.exports = pool;
